7943813145b1c9c29a15a95934e2ffe55928d49c
Delisa Mason <iskanamagus@gmail.com>
2012-07-23 15:47:15 -0400

    Support scoped bash completion

--- bin/cocoadex_completion.sh
+++ bin/cocoadex_completion.sh
@@ -17,17 +17,28 @@ class TagCompletion
   end
 
   def matches
-    tags.select do |tag|
+    completions = tags.select do |tag|
       tag[0, typed.length] == typed
     end
+
+    scope = get_scope(typed)
+    completions = completions.select do |tag|
+      get_scope(tag) == scope
+    end
+
+    completions
   end
 
   def typed
-    @command[/\s(.+?)$/, 1] || ''
+    @text ||= @command[/\s(.+?)$/, 1] || ''
   end
 
   def tags
     @tags ||= Cocoadex::Keyword.tags
+  end
+
+  def get_scope text
+    Cocoadex::Keyword::SCOPE_CHARS.detect {|c| text.include? c}
   end
 end
 
