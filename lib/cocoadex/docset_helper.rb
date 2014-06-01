525f47deefd98c05808540c5183a78808292728e
Delisa Mason <iskanamagus@gmail.com>
2014-06-01 03:51:01 -0700

    Avoid loading outdated documentation sets

--- lib/cocoadex/docset_helper.rb
+++ lib/cocoadex/docset_helper.rb
@@ -4,6 +4,9 @@ module Cocoadex
   # Helper for finding and indexing DocSets
   class DocSetHelper
 
+    IOS_DOCSET_PREFIX="com.apple.adc.documentation.AppleiOS"
+    OSX_DOCSET_PREFIX="com.apple.adc.documentation.AppleOSX"
+    XCODE_DOCSET_PREFIX="com.apple.ADC_Reference_Library.DeveloperTools"
     ROOT_PATHS = [
       '~/Library/Developer/Documentation/DocSets',
       '~/Library/Developer/Shared/Documentation/DocSets',
@@ -17,25 +20,33 @@ module Cocoadex
 
     def self.docset_paths
       @paths ||= begin
-        ROOT_PATHS.map do |path|
+        paths = ROOT_PATHS.map do |path|
           Dir.glob(File.expand_path(path)+'/*/')
-        end.flatten
+        end.flatten.sort
+
+        rejected_prefixes = [OSX_DOCSET_PREFIX, IOS_DOCSET_PREFIX, XCODE_DOCSET_PREFIX]
+        rejected_paths = rejected_prefixes.map {|p| reject_paths(paths, p)}.flatten
+        paths.reject! {|p| rejected_paths.include?(p) }
+
+
+        require 'pry'
+        binding.pry
+
+
+        paths
       end
     end
 
     def self.search_and_index paths=docset_paths
       docsets = []
+
       paths.map do |path|
         if docset = Parser.parse(path)
           docsets << docset
         end
       end
 
-      if docsets.size > 0
-        Tokenizer.persist
-        CompletionHelper.generate_tags!
-        write(docsets)
-      end
+      write(docsets) if docsets.size > 0
       logger.info "Done! #{docsets.size} DocSet#{docsets.size == 1 ? '':'s'} indexed."
     end
 
@@ -56,7 +67,17 @@ module Cocoadex
 
     def self.write docsets
       @docsets = docsets
+      Tokenizer.persist
+      CompletionHelper.generate_tags!
       Serializer.write_array(data_path, docsets, :overwrite)
+    end
+
+    private
+
+    def self.reject_paths paths, prefix
+      filtered_paths = paths.select {|p| File.basename(p).start_with?(prefix) }
+      name_to_keep = File.basename(filtered_paths.shift)
+      filtered_paths.select {|p| File.basename(p) != name_to_keep }
     end
   end
 end
